962bce30f27725e7239a74fd5976def45f2c976d
Andrzej.Skibiński <andrzej.skibinski@blare.tech>
2023-01-14 23:04:37 +0100

    adding enum for sorting modes editor gui for sorting modes hide change

--- Assets/Scripts/VirtaExerciseSorter/ComponentOrganizer.cs
+++ Assets/Scripts/VirtaExerciseSorter/ComponentOrganizer.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 
 public class ComponentOrganizer
 {
-    public List<Component> OtherComponents = new List<Component>();
-    public Component Sorter;
-    public List<Component> VirtaComponents = new List<Component>();
+    List<Component> OtherComponents = new List<Component>();
+    Component Sorter;
+    List<Component> VirtaComponents = new List<Component>();
 
-    public ComponentOrganizer(List<Component> components)
-    {
+    public List<Component> SortedComponents = new List<Component>();
+
+    public void SortByNameDescending(List<Component> components){
         components.ForEach((c) => {
             if(c is VirtaMonoBehaviour) VirtaComponents.Add(c);
             else if (c is VirtaExerciseSorter) Sorter = c;
             else OtherComponents.Add(c);
-        });        
+        });  
+        SortedComponents.AddRange(OtherComponents);
+        SortedComponents.Add(Sorter);
+        SortedComponents.AddRange(VirtaComponents);
     }
 }
